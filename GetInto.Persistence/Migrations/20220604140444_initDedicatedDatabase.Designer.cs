b1abf7982da444cb51af80094d8371b7123e8fa7
mateus-ceccagno <sk.mateus@hotmail.com>
2022-06-04 11:43:50 -0300

    migration to dedicated database

--- GetInto.Persistence/Migrations/20220604140444_initDedicatedDatabase.Designer.cs
+++ GetInto.Persistence/Migrations/20220604140444_initDedicatedDatabase.Designer.cs
@@ -4,6 +4,7 @@ using GetInto.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GetInto.Persistence.Migrations
 {
     [DbContext(typeof(GetIntoContext))]
-    partial class GetIntoContextModelSnapshot : ModelSnapshot
+    [Migration("20220604140444_initDedicatedDatabase")]
+    partial class initDedicatedDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
